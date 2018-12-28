cfa96293391cda7bf819f16e635f80bf108535c8
Diogo Cruz <diogohmcruz@hotmail.com>
2018-12-28 17:12:21 +0000

    Update Cat movement to new messages

--- src/robots/src/cat_movement.py
+++ src/robots/src/cat_movement.py
@@ -6,13 +6,13 @@ import rospy
 import numpy as np
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry, MapMetaData
-from cat_mouse_world.msg import RoboSpotted
+from cat_mouse_world.msg import RobotsSpotted
 from go_to_goal import move_to_goal
 from angular_movement import *
 from linear_movement import *
 
 
-mouse_position = RoboSpotted()
+mouse_position = RobotsSpotted()
 robot_odom = Odometry()
 
 
@@ -25,20 +25,25 @@ def odomCallback(odom_message):
 def sightCallback(sight_message):
     '''Mouse Sight memory update'''
     global mouse_position
-    mouse_position = sight_message
-    cat_movement()
+    mouse_position = closest_mouse(sight_message)
+    if mouse_position == []:
+        print 'No mouse near'
+    elif mouse_position.dist < 0.1:
+        print 'caught'
+    else:
+        cat_movement()
 
 
 def stop():
-    mouse_position = RoboSpotted()
+    mouse_position = []
     velocity_publisher.publish(Twist())
 
 
 def go_to_goal(x_goal, y_goal):
     '''PID Controller'''
     global robot_odom
-    distance = 0.2 + 1
-    while distance > 0.2 and not rospy.is_shutdown():
+    distance = 1
+    while distance > 0.1 and not rospy.is_shutdown():
         velocity_message, distance = move_to_goal(robot_odom, x_goal, y_goal)
         velocity_publisher.publish(velocity_message)
         rate.sleep()
@@ -50,17 +55,21 @@ def subscribers(cat_name):
     rospy.Subscriber(position_topic, Odometry, odomCallback)
 
     sight_topic = '/' + cat_name + '/sight'
-    rospy.Subscriber(sight_topic, RoboSpotted, sightCallback)
+    rospy.Subscriber(sight_topic, RobotsSpotted, sightCallback)
+
+
+def closest_mouse(sight_message):
+    visible_mice = sight_message.robotsSpotted
+    return min(visible_mice, key=lambda x: x.dist) if len(visible_mice) > 0 else []
 
 
 def cat_movement():
     global mouse_position, robot_odom
-    saw_mouse = mouse_position != RoboSpotted()
+    saw_mouse = mouse_position != []
     if saw_mouse:
         position = robot_odom.pose.pose.position
         x_goal = position.x + mouse_position.dist * cos(mouse_position.angle)
         y_goal = position.y + mouse_position.dist * sin(mouse_position.angle)
-        print x_goal, y_goal
         go_to_goal(x_goal, y_goal)
     else:
         move(velocity_publisher, 3, 0.5, True)
